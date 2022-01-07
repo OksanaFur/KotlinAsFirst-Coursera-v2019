7c1f5dce67901673f512f0b5fa0ea0f64b7d3cd9
Oksana Furman <oksana-prokhodenko@ya.ru>
2022-01-08 00:30:31 +0400

    solved 4th week besides last one

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -341,8 +341,37 @@ fun decimalFromString(str: String, base: Int): Int {
  * Римские цифры: 1 = I, 4 = IV, 5 = V, 9 = IX, 10 = X, 40 = XL, 50 = L,
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
+ * | 1000, 1000, 1000, 100, 100, 40, 1, 1, 1
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    val limNumbers = listOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    val list = mutableListOf<Int>()
+    var newN = n
+    for (limNumber in limNumbers) {
+        while (newN >= limNumber) {
+            list.add(limNumber)
+            newN -= limNumber
+        }
+    }
+    return list.map {
+        when (it) {
+            1000 -> "M"
+            900 -> "CM"
+            500 -> "D"
+            400 -> "CD"
+            100 -> "C"
+            90 -> "XC"
+            50 -> "L"
+            40 -> "XL"
+            10 -> "X"
+            9 -> "IX"
+            5 -> "V"
+            4 -> "IV"
+            1 -> "I"
+            else -> ""
+        }
+    }.joinToString(separator = "")
+}
 
 /**
  * Очень сложная
